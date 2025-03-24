532974ab0b51341bda28d0729173ccf66afa6297
DevYayaFarias <minvicflg@gmail.com>
2025-03-24 09:26:59 -0300

    Create teste.py

---
+++ teste.py
@@ -0,0 +1,14 @@
+amor = input("Defina amor: ")
+perdão = input("O que ele fez por ti? ")
+print("A graça é o amor e o perdão de Jesus, que nos salvou por meio do seu sacrifício.!!")
+print("\nVocê já experimentou um amor assim?")
+print("Como o perdão transformou sua vida?")
+print("Você acredita que merece essa graça?")
+
+print(
+    "\nJoão 3:16: 'Porque Deus amou o mundo de tal maneira que deu o seu Filho unigênito, para que todo aquele que nele crê não pereça, mas tenha a vida eterna.'")
+
+print("\n 2 Coríntios 5:17: 'Portanto, se alguém está em Cristo, é uma nova criação. As coisas antigas já passaram; eis que se fizeram novas!")
+
+print(" Jesus está batendo à porta, esperando você abrir para que ele possa tranformar toda sua vida. Não importa o seu passado, "
+      "não importa o que vc já fez, ele te ama e quer te curar. Abra a porta, vale a pena renunciar tudo por Cristo!!")
